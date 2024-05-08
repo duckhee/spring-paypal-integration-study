b0c076ae10e0768b132bdde4e538091cddf8295b
DoukheeWon <fain9301@yahoo.com>
2024-05-08 22:02:24 +0900

    feat: first commit

---
+++ src/main/java/kr/co/won/springpaypalintegrationstudy/SpringPaypalIntegrationStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.springpaypalintegrationstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalIntegrationStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPaypalIntegrationStudyApplication.class, args);
+    }
+
+}
